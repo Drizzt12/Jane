bfb9baa293afacad34810ef75016b4892405efc3
Samuel Mulder <smulder2000@yahoo.com>
2017-12-26 16:36:33 -0700

    Initial Jane gui commit.

---
+++ jane_gui.py
@@ -0,0 +1,16 @@
+
+import Tkinter as tk 
+
+class Jane_Gui:
+	def __init__(self, master):
+		frame = tk.Frame(master)
+		frame.pack()
+		textbox = tk.Text(frame)
+		textbox.pack()
+		textbox.insert(tk.END, "Welcome to the Jane GUI!")
+
+root = tk.Tk()
+jane_gui = Jane_Gui(root)
+
+root.mainloop()
+root.destroy()
