700edc1b6b5dee758ac2add666cf3b89c2ce2726
Eirik Mulder <eirik.mulder@gmail.com>
2017-12-26 17:48:23 -0700

    GigaByte Formatting
    Format data to gigabytes

--- system_stats.py
+++ system_stats.py
@@ -12,16 +12,20 @@ def sys_stats():
 		cpus = psutil.cpu_count(logical=False)
 		vcpus = psutil.cpu_count()
 		cpu_speed = psutil.cpu_freq().current
-		memory = psutil.virtual_memory().total
+		memory = psutil.virtual_memory().total/1024
+		memory = memory/1024
+		memory = memory/1024
 		percent_mem = psutil.virtual_memory().percent
-		free_disk = psutil.disk_usage('/').free
+		free_disk = psutil.disk_usage('/').free/1024
+		free_disk = free_disk/1024
+		free_disk = free_disk/1024
 
 		outstr += "Physical CPUS: " + str(cpus) + "\n"
 		outstr += "Virtual CPUS: " + str(vcpus) + "\n"
 		outstr += "CPU speed: " + str(cpu_speed) + " mhz \n"
-		outstr += "Virtual Memory: " + str(memory) + " bytes \n"
+		outstr += "Virtual Memory: " + str(memory) + " gigabytes \n"
 		outstr += "Percent free: " + str(percent_mem) + "%\n"
-		outstr += "Disk space free: " + str(free_disk) + " bytes \n"
+		outstr += "Disk space free: " + str(free_disk) + " gigabytes \n"
 
 		return outstr
 
