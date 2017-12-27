0d8f8456557aa99fff7d27d8384f65202276a5d1
Samuel Mulder <smulder2000@yahoo.com>
2017-12-26 17:27:45 -0700

    Printing system stats.

--- jane_gui.py
+++ jane_gui.py
@@ -9,7 +9,11 @@ class Jane_Gui:
 		frame.pack()
 		textbox = tk.Text(frame)
 		textbox.pack()
-		textbox.insert(tk.END, "Welcome to the Jane GUI!")
+		textbox.insert(tk.END, "Welcome to the Jane GUI! \n\n")
+		textbox.insert(tk.END, "Your system stats: \n")
+		stat_str = system_stats.sys_stats()
+		textbox.insert(tk.END, stat_str)
+		textbox.insert(tk.END, "\n\n Pretty decent system!")
 
 root = tk.Tk()
 root.title("Jane")
--- system_stats.py
+++ system_stats.py
@@ -7,6 +7,24 @@ except:
 
 def sys_stats():
 	if utils_found:
-		return "Got it."
+		outstr = ""
+
+		cpus = psutil.cpu_count(logical=False)
+		vcpus = psutil.cpu_count()
+		cpu_speed = psutil.cpu_freq().current
+		memory = psutil.virtual_memory().total
+		percent_mem = psutil.virtual_memory().percent
+		free_disk = psutil.disk_usage('/').free
+
+		outstr += "Physical CPUS: " + str(cpus) + "\n"
+		outstr += "Virtual CPUS: " + str(vcpus) + "\n"
+		outstr += "CPU speed: " + str(cpu_speed) + " mhz \n"
+		outstr += "Virtual Memory: " + str(memory) + " bytes \n"
+		outstr += "Percent free: " + str(percent_mem) + "%\n"
+		outstr += "Disk space free: " + str(free_disk) + " bytes \n"
+
+		return outstr
+
+
 	else:
-		return "Nope."
+		return "psutil not installed.  Use: pip install psutil"
